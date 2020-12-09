af5b2ca51757e8f2cb118d113c6561d7c6256022
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-12-09 22:51:01 +0500

    Create Decrypt.py
    Requirements:
    
    Python3

---
+++ Decrypt.py
@@ -0,0 +1,31 @@
+import sys
+
+sys.ps1 = '\033[94m'
+
+print(sys.ps1)
+
+print('''
+ ████████╗██████╗ ██████╗ ██████╗ 
+╚══██╔══╝██╔══██╗╚════██╗╚════██╗
+   ██║   ██████╔╝ █████╔╝ █████╔╝
+   ██║   ██╔══██╗ ╚═══██╗ ╚═══██╗
+   ██║   ██║  ██║██████╔╝██████╔╝
+   ╚═╝   ╚═╝  ╚═╝╚═════╝ ╚═════╝ 
+                                 
+                       
+''')
+
+msg = input('Enter the message ya wanna decrypt: ')
+
+algorithm = 'elfghijkzmadnopcqrbstuvwxy'
+
+key = int(input('Enter your key: '))
+
+
+dec = ''
+for i in msg:
+    ps = algorithm.find(i)
+    newps = (ps-key)%26
+    dec +=algorithm[newps]
+    
+print(dec)
