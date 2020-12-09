c4a1c723f92fff2bdead8e40d60a1955b7601123
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-12-09 22:50:09 +0500

    Create encrypt.py
    Requirements:
    
    Python3

---
+++ encrypt.py
@@ -0,0 +1,33 @@
+import sys
+
+
+sys.ps1 = '\033[94m'
+
+print(sys.ps1)
+
+print('''
+ ████████╗██████╗ ██████╗ ██████╗ 
+╚══██╔══╝██╔══██╗╚════██╗╚════██╗
+   ██║   ██████╔╝ █████╔╝ █████╔╝
+   ██║   ██╔══██╗ ╚═══██╗ ╚═══██╗
+   ██║   ██║  ██║██████╔╝██████╔╝
+   ╚═╝   ╚═╝  ╚═╝╚═════╝ ╚═════╝ 
+                                 
+                       
+''')
+
+
+msg = input('Enter the message ya wanna encrypt: ')
+
+algorithm = 'elfghijkzmadnopcqrbstuvwxy'
+
+key = int(input('Enter your key: '))
+
+
+enc = ''
+for i in msg:
+    ps = algorithm.find(i)
+    newps = (ps+key)%26
+    enc +=algorithm[newps]
+    
+print(enc)
